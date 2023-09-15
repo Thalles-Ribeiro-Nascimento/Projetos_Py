e928c887853e837378028c45b68304a7b4315453
thalles <thalles.nascimentorj@gmail.com>
2023-09-15 10:51:06 -0300

    Projetos em python

--- baskhara.py
+++ baskhara.py
@@ -11,16 +11,19 @@ a = float(input('Insira um valor: '))
 b = float(input('Insira um valor: '))
 c = float(input('Insira um valor: '))
 
+print(f'A equação ficou {a:.0f}x² + {b:.0f}x + {c:.0f} = 0')
 #Variável delta
-
 delta = b**2 - 4 * a * c
 
-print(f'Delta = {delta}')
+print(f'Delta = {b:.0f}² - 4 x {a:.0f} x {c:.0f}\n'
+      f'Delta = {delta}')
+
 
 #Raízes
 x1 = (-b + delta**(1/2))/2*a
-
+print(f'x1 = -{b:.0f} + raiz de {delta}/2x{a:.0f}')
 x2 = (-b - delta**(1/2))/2*a
+print(f'x2 = -{b:.0f} - raiz de {delta}/2x{a:.0f}')
 
 #Condições
 if a == 0:
@@ -30,7 +33,8 @@ elif delta < 0:
     print('Impossível calcular a raiz!! O valor do delta não pode ser menor que zero.')
 
 else:
-    print(f'x1 = {x1}; x2 = {x2}')
+    print(f'x1 = {x1:.2f}\n'
+          f'x2 = {x2:.2f}')
 
 #Fim
 print('Obrigado por usar nosso programa. Volte sempre!!')
